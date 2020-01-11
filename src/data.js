22fd85cb5b18b9489d729432931369b2108d93cc
John Boyle <nikolasboyle@gmail.com>
2020-01-11 13:14:30 -0800

    Published solution 6

--- src/data.js
+++ src/data.js
@@ -168,5 +168,43 @@ const solve = (min, max) => {
 solve(1, 20)`,
 			codepen: "https://codepen.io/clockworkftw-the-sasster/pen/povaYRE"
 		}
+	},
+	{
+		id: 6,
+		title: "Sum square difference",
+		prompt: [
+			"The sum of the squares of the first ten natural numbers is,",
+			"1^2+2^2+...+10^2=385",
+			"The square of the sum of the first ten natural numbers is,",
+			"(1+2+...+10)^2=55^2=3025",
+			"Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025−385=2640.",
+			"Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum."
+		],
+		solution: {
+			value: "25164150",
+			explaination: [],
+			code: `const sumOfSquares = max => {
+	let squares = []
+	for (let i = 1; i <= max; i++) {
+		squares.push(i*i)  
+	}
+	return squares.reduce((acc, cur) => acc + cur)
+}
+
+const squareOfSums = max => {
+	let sum = 0
+	for (let i = 1; i <= max; i++) {
+		sum += i
+	}
+	return sum * sum
+}
+
+const solve = max => {
+	return squareOfSums(max) - sumOfSquares(max)
+}
+
+solve(100)`,
+			codepen: "https://codepen.io/clockworkftw-the-sasster/pen/povaYRE"
+		}
 	}
 ];
